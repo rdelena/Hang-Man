49736b5226e943f3a419fe6776c51f4e30d9fe07
rdelena <rodnydelena@gmail.com>
2023-02-20 19:59:23 -0600

    final touches worked with partner

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
-# Hang-Man
+Code Plan
 
-create a Hangman game where letters are chosen to complete a predetermined word.  
+-create a Hangman game where letters are chosen to complete a predetermined word.
 
-Reveal a letter from a word if a user inputs that letter and it exists in the word. If it doesn't let the user know
+-use get prompt () function to let user input guesses
 
-number of lives
+-The word bank will store all the random words that will be used for the player to guess.
 
-board function to show the amount of letters on random word
+-create a function to generate a random word out our array of words using math.floor and math.random method to cycle through array
+
+-Reveal a letter from a word if a user inputs that letter and it exists in the word. If it doesn't let the user know number of lives remaining out of 6
+
+-check for win function checks to make sure letters match with random word after every turn
+
+-board function to show the amount of letters on random word using \_ in an array
+
+-We need to change every letter in the array.length of the random word so that players are not able to see what the word is. How many plans should the player have? We decided on 6.
+
+-create ascii diagram for terminal in order to see the classic hang man/body part image appear after every wrong guess
+
+-show what word was when you lose
--- main.js
+++ main.js
@@ -7,10 +7,6 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-// var name = readline.question("What is your name? ");
-
-// console.log("Hi " + name + ", nice to meet you.");
-
 const wordBank = [
   "variable",
   "terminal",
@@ -29,38 +25,46 @@ const wordBank = [
   "parse",
 ];
 
+// Uses the word bank to randomize array and choose a word for the hangman game.
 let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-console.log(randomWord);
+// console.log(randomWord) to show answer in terminal
 
+// creates a new array that pushes the random word and changes each letter into an underscore "_"
 let board = new Array(randomWord.length).fill("_");
 console.log(board);
 
+// sets the number lives for the player lives
 let lives = 6;
 
+// takes the input of the player
 const guess = (input) => {
+  // loops through the randomWord array checks if input is equal to the
   for (let i = 0; i < randomWord.length; i++) {
     if (input === randomWord[i]) {
+      // sets board array as input
       board[i] = input;
     }
   }
+  // condition if random word is not found
   if (!randomWord.includes(input)) {
     console.log("You have lost a life");
+    // subtracts a life
     lives--;
-
     console.log(`You have ${lives} left`);
   }
-
+  // console.log(hangManPics[lives])
   console.log(board);
 };
 
+// If board still has an underscore string "_"
 const checkForWin = () => {
   if (!board.includes("_")) {
-    console.log(`You won!`);
+    console.log(`YOU WON! ☜(ﾟヮﾟ☜)`);
   } else if (lives === 0) {
-    console.log("You lose!");
+    console.log(`You lose! The word was: ${randomWord}`);
   }
 };
-// Checks how many lives the player has and populates a hangman figure for each life.
+// Checks how many lives the player has and populates a hangman figure for each life taken
 function checkNumLives() {
   if (lives === 6) {
     console.log(
@@ -155,12 +159,14 @@ function checkNumLives() {
   }
 }
 
+// hangMan function runs other functions and takes an answer as a parameter
 const hangMan = (answer) => {
   guess(answer);
   checkNumLives();
   checkForWin();
 };
 
+// shows the prompt for users to see instruction and type their guesses
 const getPrompt = () => {
   rl.question("Guess a letter! ", (answer) => {
     hangMan(answer);
